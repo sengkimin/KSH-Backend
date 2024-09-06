fb303d8261efb0520bfc157fad5a785ae70f53b7
ChhunlinOn <chhunlin.on@institute.pse.ngo>
2024-09-06 13:42:03 +0700

    update resident medical

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,19 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface DocumentDocument extends Schema.Component {
+  collectionName: 'components_document_documents';
+  info: {
+    displayName: 'document';
+  };
+  attributes: {
+    document_type: Attribute.Enumeration<
+      ['Identity Card', 'Family Book', 'Birth Certificate', 'Poor ID Card']
+    >;
+    description: Attribute.Text;
+    file_media: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
+  };
+}
+
 export interface ActivityActivity extends Schema.Component {
   collectionName: 'components_activity_activities';
   info: {
@@ -17,25 +31,11 @@ export interface ActivityActivity extends Schema.Component {
   };
 }
 
-export interface DocumentDocument extends Schema.Component {
-  collectionName: 'components_document_documents';
-  info: {
-    displayName: 'document';
-  };
-  attributes: {
-    document_type: Attribute.Enumeration<
-      ['Identity Card', 'Family Book', 'Birth Certificate', 'Poor ID Card']
-    >;
-    description: Attribute.Text;
-    file_media: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'activity.activity': ActivityActivity;
       'document.document': DocumentDocument;
+      'activity.activity': ActivityActivity;
     }
   }
 }
